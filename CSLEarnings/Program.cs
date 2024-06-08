622f64d666c2fb2d5bdecf9b1d44a343901a34fa
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-06-09 00:07:52 +0300

    Добавьте файлы проекта.

---
+++ CSLEarnings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLEarnings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello map!");
+            Console.ReadLine();
+        }
+    }
+}
