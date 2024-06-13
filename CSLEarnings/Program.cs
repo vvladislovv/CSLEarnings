9cbdeb096282df597a787e6ae9e293ca3a724b7c
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-06-13 12:02:20 +0300

    df

--- CSLEarnings/Program.cs
+++ CSLEarnings/Program.cs
@@ -10,9 +10,23 @@ namespace CSLEarnings
     {
         static void Main(string[] args)
         {
-            int i = 0;
-           // ++i;
-            Console.WriteLine((++i) + 2 + 1 + (i++) + "1"+ (++i)  * 2);
+            float Health;
+            int armor;
+            int damage;
+            int precentConvert = 100;
+
+            Console.Write("Введите количество здоровья: ");
+            Health = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите количество брони: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите количество урона: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            Health -= Convert.ToSingle(damage) / precentConvert * armor;
+
+            Console.WriteLine($"Вам нанесли {damage} урона. У нас осталось {Health} здоровья.");
         }
     }
 }
