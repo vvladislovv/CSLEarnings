395854001fed101fdcc465b40ce54b701f9a88a3
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-06-13 15:18:06 +0300

    cxzcvzxcfasdgasdg

--- CSLEarnings/Program.cs
+++ CSLEarnings/Program.cs
@@ -10,23 +10,20 @@ namespace CSLEarnings
     {
         static void Main(string[] args)
         {
-            int money;
-            int food;
-            int foodCostPrice = 10;
-            bool isAbleToPay;
+            int age;
+            Console.Write("Введите свой возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Welcome in pecarnia! Today eat zby {foodCostPrice} money.");
-            Console.Write("How many money do you have?");
-            money = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("How much food do you need?");
-            food = Convert.ToInt32(Console.ReadLine());
-
-            isAbleToPay = money >= food * foodCostPrice;
-            food *= Convert.ToInt32(isAbleToPay);
-            money -= food * foodCostPrice;
-            Console.WriteLine($"In your bag {food} food units, and {money} money.");
-
+            if (age >= 18)
+            {
+                Console.WriteLine("Добро пожайловать в нащ бар!");
+                Console.WriteLine("Что будете пить?");
+            }
+            else
+            {
+                Console.WriteLine("Вы слишком малы");
+                Console.WriteLine($"Приходите к нам через: {18 - age} лет!");
+            }
 
         }
     }
