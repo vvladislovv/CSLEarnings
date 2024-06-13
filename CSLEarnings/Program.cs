34e9d4df030a481ea6c192f1e46365b4f76bd5e7
Vladislav Elcheninov <133252067+vvladislovv@users.noreply.github.com>
2024-06-13 12:02:20 +0300

    test 1

--- CSLEarnings/Program.cs
+++ CSLEarnings/Program.cs
@@ -10,9 +10,22 @@ namespace CSLEarnings
     {
         static void Main(string[] args)
         {
-            int i = 0;
-           // ++i;
-            Console.WriteLine((++i) + 2 + 1 + (i++) + "1"+ (++i)  * 2);
+            int money;
+            int food;
+            int foodCostPrice = 10;
+            bool isAbleToPay;
+
+            Console.WriteLine($"Welcome in pecarnia! Today eat by {foodCostPrice} money.");
+            Console.Write("How many money do you have?");
+            money = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How much food do you need?");
+            food = Convert.ToInt32(Console.ReadLine());
+
+            isAbleToPay = money >= food * foodCostPrice;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * foodCostPrice;
+            Console.WriteLine($"In your bag {food} food units, and {money} money.");
         }
     }
 }
